665397aa24480a904746aa01b7df6aaa35360345
Lucia Azubel <Lucia_24_azubel@hotmail.com>
2022-12-11 16:53:48 -0300

    creo el maquetado de la pag de administrador, la tabla e info de las peliculas a almacenar, y guardo esa info en localstorage

---
+++ js/classPelicula.js
@@ -0,0 +1,11 @@
+export class Pelicula{
+    constructor(codigo, titulo, descripcion, imagen, genero){
+        this.codigo=codigo;
+        this.titulo=titulo;
+        this.descripcion=descripcion;
+        this.imagen=imagen;
+        this.genero=genero;
+    }
+
+    // crear metodo set y get para cada unade las propiedades 
+}
